ee46bb080521c0efb53493cb84dd71e13346522f
Cristina Zhang <rami0141@umn.edu>
2018-08-21 15:58:19 -0500

    routes added

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,21 @@
 import React from 'react';
-import Nav from './components/Nav';
-import Footer from './components/Footer';
-import Form from './components/Form';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./pages/Home";
+import NoMatch from "./pages/NoMatch";
+import Saved from "./pages/Saved";
+import Nav from "./components/Nav";
 
 const App = () => (
-  <div>
-    <Nav />
-    <Form />
-    <Footer />
-  </div>
+  <Router>
+    <div>
+      <Nav />
+      <Switch>
+        <Router exact path="/" component={Home} />
+        <Router exact path="/saved" component={Saved} />
+        <Router component={NoMatch} />
+      </Switch>
+    </div>
+  </Router>
 );
 
 export default App;
--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -1,23 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Nav.css";
 
 const Nav = () => (
   <nav className="navbar navbar-expand-lg">
     <div className="container">
-    <a className="navbar-brand ml-4 text-info" href="#">NYT React</a>
+    <Link className="navbar-brand ml-4 text-info" to="/">NYT React</Link>
     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarText">
       <ul className="navbar-nav mr-auto">
-        <li className="nav-item active">
-          <a className="nav-link text-info" href="#">Home</a>
+        <li className="nav-item">
+          <Link className="nav-link text-info mr-4" to="/">SEARCH</Link>
         </li>
         <li className="nav-item">
-          <a className="nav-link text-info" href="#">NoMatch</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link text-info" href="#">Saved</a>
+          <Link className="nav-link text-info" to="/saved">SAVED ARTICLES</Link>
         </li>
       </ul>
     </div>
